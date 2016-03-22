74ec2530ff0070149651cf8b96e0e47437e4e97c
Epsy <test@test.com>
2016-03-22 15:18:03 +0000

    echo sounder subscriber

--- src/manta/scripts/position_subscriber.py
+++ src/manta/scripts/position_subscriber.py
@@ -2,13 +2,15 @@
 import rospy
 import time
 from sensor_msgs.msg import NavSatFix
-#from manta.msg import Imu
-from sensor_msgs.msg import Imu
+from manta.msg import Imu
+#from sensor_msgs.msg import Imu
 from sensor_msgs.msg import LaserScan
+from manta.msg	 import Echo
 import numpy
 
 isGpsWritten = False
-isTelemeterWritten = False
+#isTelemeterWritten = False
+isEchosounderWritten = False
 isImuWritten = False
 isTimeWritten = False
 currentTime = 0
@@ -23,7 +25,8 @@ def isClose(t1, t2):
 def resetAll():
 	rospy.loginfo("---- RESET DATA ----")
 	global isGpsWritten
-	global isTelemeterWritten
+	#global isTelemeterWritten
+	global isEchosounderWritten	
 	global isImuWritten
 	global isTimeWritten
 	global currentTime
@@ -31,7 +34,8 @@ def resetAll():
 	
 	isGpsWritten = False
 	isImuWritten = False
-	isTelemeterWritten = False
+	#isTelemeterWritten = False
+	isEchosounderWritten = False	
 	isTimeWritten = False
 
 	results = open("results.txt", "a")
@@ -39,14 +43,15 @@ def resetAll():
 def general_callback(data):
 	#use global variables
 	global isGpsWritten
-	global isTelemeterWritten
+	#global isTelemeterWritten
+	global isEchosounderWritten
 	global isImuWritten
 	global isTimeWritten
 	global currentTime
 	global tempTime
 
 	results = open("results.txt", "a")
-	if(isGpsWritten == False and isTelemeterWritten == False and isImuWritten == False and isTimeWritten == False):
+	if(isGpsWritten == False and isEchosounderWritten == False and isImuWritten == False and isTimeWritten == False):
 		currentTime = int(round(time.time()*1000000000))
 		results.write("T ")
 		# write time in milliseconds
@@ -76,30 +81,44 @@ def general_callback(data):
 				isGpsWritten = True
 			else:
 				results.close()
-				resetAll()		
+				resetAll()
 	else:
-		tempTime = int(round(time.time()*1000000000))	
-		if(isGpsWritten == True and isImuWritten == True and isTelemeterWritten == False): 
+		tempTime = int(round(time.time()*1000000000))
+		if(isGpsWritten == True and isImuWritten == True and isEchosounderWritten == False):
 			if(isClose(currentTime, tempTime) == True):
-				rospy.loginfo("---- TELEMETER WRITE ----")
-				tableLength = len(data.ranges)
-				results.write(" DI ")
-				results.write(str(data.ranges[tableLength-1]))
-				results.write(" ")
-				results.write(str(data.ranges[int(numpy.floor(tableLength/2))]))
-				results.write(" ")
-				results.write(str(data.ranges[0]))
-				isTelemeterWritten = True
+				rospy.loginfo("---- ECHO SOUNDER WRITE ----")
+				results.write(" PRO ")
+				results.write(str(data.profondeur))
+				isEchosounderWritten = True
 			else:
 				results.close()
 				resetAll()
+#		
+#	else:
+#		tempTime = int(round(time.time()*1000000000))	
+#		if(isGpsWritten == True and isImuWritten == True and isTelemeterWritten == False): 
+#			if(isClose(currentTime, tempTime) == True):
+#				rospy.loginfo("---- TELEMETER WRITE ----")
+#				tableLength = len(data.ranges)
+#				results.write(" DI ")
+#				results.write(str(data.ranges[tableLength-1]))
+#				results.write(" ")
+#				results.write(str(data.ranges[int(numpy.floor(tableLength/2))]))
+#				results.write(" ")
+#				results.write(str(data.ranges[0]))
+#				isTelemeterWritten = True
+#			else:
+#				results.close()
+#				resetAll()
+
 	# all the data has been written	
-	if(isGpsWritten == True and isImuWritten == True and isTelemeterWritten == True):
+	if(isGpsWritten == True and isImuWritten == True and isEchosounderWritten == True):
 		rospy.loginfo("---- LINE WRITTEN ----")
 		results.write("\n")		
 		isGpsWritten = False
 		isImuWritten = False
-		isTelemeterWritten = False
+		isEchosounderWritten = False
+		#isTelemeterWritten = False
 		isTimeWritten = False
 	# Close file	
 	results.close()
@@ -107,7 +126,8 @@ def listener():
 	rospy.init_node('listener', anonymous=True)
 	rospy.Subscriber('/gps/fix', NavSatFix, general_callback)
 	rospy.Subscriber('/imu/data', Imu, general_callback)
-	rospy.Subscriber('scan', LaserScan, general_callback)
+	rospy.Subscriber('echo/data', Echo, general_callback)	
+	#rospy.Subscriber('scan', LaserScan, general_callback)
 	rospy.spin()
 
 if __name__ == '__main__':
